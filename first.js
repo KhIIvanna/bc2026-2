8cb17391c281223b5d2f3c7fa0f5ef09831a1879
Ivanna web2026-2 <khilinska.ivanna@gmail.com>
2026-02-22 22:01:51 +0200

    Changed first.json

--- first.js
+++ first.js
@@ -1,10 +1,10 @@
 const date = "202302";
 const time_period = "m";
 
-const nbuUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/cpi");
+const nbuUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation");
 
-nbuUrl.searchParams.set("date", date);
 nbuUrl.searchParams.set("period", time_period);
+nbuUrl.searchParams.set("date", date);
 nbuUrl.hash = "json";
 
 console.log(nbuUrl.toString());
