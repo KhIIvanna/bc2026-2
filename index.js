ca11325ecf680abf292ae3d320e6f5630edbd2b6
Ivanna bc2026-7 <khilinska.ivanna@gmail.com>
2026-05-13 18:08:11 +0300

    Centralization of data in one file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Command } = require('commander');
 const fs = require('fs');
 const path = require('path');
@@ -14,9 +15,7 @@ const program = new Command();
 program
   .requiredOption('-h, --host <host>', 'адреса сервера')
   .requiredOption('-p, --port <port>', 'порт сервера')
-  .requiredOption('-c, --cache <path>', 'шлях до директорії для кешу')
-  .showHelpAfterError()
-  .showSuggestionAfterError();
+  .requiredOption('-c, --cache <path>', 'шлях до директорії для кешу');
 
 program.parse(process.argv);
 const options = program.opts();
